76163f9dcb1d989b38cbcd0478acc1425ac5d3ce
KaysPart <particulier.flore@gmail.com>
2022-02-22 20:36:42 +0100

    feat: bdd

--- index.php
+++ index.php
@@ -12,9 +12,13 @@ if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $phone = $_POST['phone'];
  
-    require_once './connect.php';
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=beautyelegance;charset=UTF8','root');
+    } catch (PDOException $e){
+        echo 'Echec de la connexion : ' . $e->getMessage();
+    }
 
-    $req=$db->prepare("INSERT INTO `newsletter` (`nom`, `email`, `telephone`) VALUES(:firstname, :email, :telephone NOW())");
+    $req = $db->prepare("INSERT INTO `newsletter` (`nom`, `email`, `telephone`) VALUES(:firstname, :email, :phone NOW())");
 
     $req->bindParam('firstname',$firstname, PDO::PARAM_STR);
     $req->bindParam('email',$email, PDO::PARAM_STR);
